6b9c40ed027386e82f56937ec0113e0688b022e7
Eftakhairul Islam <eftakhairul@gmail.com>
2014-07-22 16:26:07 -0400

    Route file is updated for new resources

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   resources :urls
 
+  #Authentication
   devise_for :users
+
+  root :to => 'urls#new'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
