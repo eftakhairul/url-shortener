270858c811e46c3c7f73606ce11db77643f482cf
Eftakhairul Islam <eftakhairul@gmail.com>
2014-07-22 20:03:28 -0400

    generate_unique_key is done

--- app/models/url.rb
+++ app/models/url.rb
@@ -3,9 +3,22 @@ class Url
 
   field :url, type: String
   field :unique_key, type: String
+  field :url_id, type: Integer
   field :hits, type: Integer, default: 0
   field :created_at, :type => Date, default: Date.today
 
   validates_presence_of :url
   belongs_to :user
+
+
+  after_save do |document|
+    max_id =  self.max(:url_id)
+    document.url_id = max_id + 1
+    document.update_attributes(first_nameurl_id: max_id)
+  end
+
+  def generate_unique_key
+    max_id = self.max(:url_id)
+    max_id.to_s(36)
+  end
 end
